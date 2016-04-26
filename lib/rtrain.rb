02dac5835efa3ca9d5bf02966b58626a02ed7d36
Lynnd335 <lynnd335@gmail.com>
2016-04-26 17:03:31 -0400

    Update rtrain.rb

--- lib/rtrain.rb
+++ lib/rtrain.rb
@@ -1,7 +1,10 @@
 module RTrain
  def self.run
     home = Dir.home
-    new_css = open(home + '/.rbenv/versions/2.2.3/lib/ruby/gems/2.2.0/gems/rtrain-0.0.7/lib/generators/templates/rtrain_scaffold.scss').read
+    home = Dir.chdir(home)
+    new_css = File.join("**", "rtrain_scaffold.scss")
+    new_css = Dir.glob(new_css)[0]
+    new_css = open(new_css).read
     scaff = open('app/assets/stylesheets/scaffolds.scss', 'w')
     scaff.write(new_css)
     scaff.close
