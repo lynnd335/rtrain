929fce4ad33d26c89f6395e901b590ae247e20b8
Bryan Mytko <bryanmytko@gmail.com>
2016-04-27 15:20:02 -0400

    Move main functionality to generator

--- lib/rtrain.rb
+++ lib/rtrain.rb
@@ -1,10 +1,2 @@
-module RTrain
-  def self.run
-    new_css = File.join("**", "rtrain_scaffold.scss")
-    new_css = Dir.glob(new_css)[0]
-    new_css = open(new_css).read
-    scaff = open('app/assets/stylesheets/scaffolds.scss', 'w')
-    scaff.write(new_css)
-    scaff.close
-  end
+module Rtrain
 end
