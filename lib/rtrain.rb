cfbf40bd1e6842e1db0d63fb2c97e42d2e559774
Bryan Mytko <bryanmytko@gmail.com>
2016-04-27 10:34:07 -0400

    Remove unused variables

--- lib/rtrain.rb
+++ lib/rtrain.rb
@@ -1,12 +1,10 @@
 module RTrain
- def self.run
-    home = Dir.home
-    home = Dir.chdir(home)
+  def self.run
     new_css = File.join("**", "rtrain_scaffold.scss")
     new_css = Dir.glob(new_css)[0]
     new_css = open(new_css).read
     scaff = open('app/assets/stylesheets/scaffolds.scss', 'w')
     scaff.write(new_css)
     scaff.close
- end 
+  end
 end
