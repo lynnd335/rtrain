479231e0667ca5b74ef04b3adf9d94b7380c2745
Dan Lynn <lynnd335@gmail.com>
2016-05-12 19:09:20 -0400

    Now 17% less awful

--- README.md
+++ README.md
@@ -2,23 +2,31 @@
 ![Ugh](http://www.heyridge.com/wp-content/uploads/2015/03/File-2012-12-29-R-train.jpg)
 _Ugh._
 
-#Rails UI Booster
-Turns your basic scaffold into something that's at least tolerable to look at... _More to come!_
-
-![Ugh](http://i.imgur.com/xzbeMWC.png)
-_Oooooooooooooo!_
+#Rails Downloadable Content (DLC) Expansion Pack
+Adds better CSS to scaffold, HTML navigation, more features are in the pipe, so stay tuned ... _DLC Season Pass Coming Soon Too!_
 
 How to use RTrain
 
 **Put this in your Gemfile**
 ```
-gem 'rtrain', '~> 0.1.5'
+gem 'rtrain', '~> 0.1.6'
 ```
 Then `bundle install`
 
-**After generating a scaffold, enter this into the command line:**
+**After generating a scaffold, enter any of these into the command line:**
 
 ```
-rails generate rtrain:install
+rails generate rtrain:install --copy_css
 ```
+![Ugh](http://i.imgur.com/xzbeMWC.png)
+_Oooooooooooooo!_
+
+Turns your basic scaffold into something that's at least tolerable to look at!
+
+**New! Add a sweet nav bar!**
+```
+rails generate rtrain:install --copy_css
+```
+![Ugh](http://i.imgur.com/hywhd0t.png)
+
 **That's it! Now your Rails application is equipped with an RTrain!**
--- lib/generators/rtrain/install_generator.rb
+++ lib/generators/rtrain/install_generator.rb
@@ -2,36 +2,89 @@ module Rtrain
   module Generators
     class InstallGenerator < Rails::Generators::Base
       desc "Creates a generator for Rtrain to style scaffold tables."
+      class_option :copy_css, type: :boolean, desc: 'Write some nice CSS for scaffold'
+      class_option :add_nav, type: :boolean, desc: 'Makes a wicked sweet nav bar for your rails app'
 
-      def copy_css
-        dir = File.dirname(__FILE__)
-
-        scaffold_css = "../templates/rtrain_scaffold.scss"
-        new_css_path = File.join(dir, scaffold_css)
-        new_css = open(new_css_path).read
-
-        old_css = open("app/assets/stylesheets/scaffolds.scss", "w")
-        old_css.write(new_css)
-        old_css.close
-
-        # rtrain_applayout_erb = "../templates/rtrain_application.html.erb"
-        # new_applayout_path = File.join(dir, rtrain_applayout_erb)
-        # new_applayout = open(new_applayout_path).read
-
-        old_applayout = open("app/views/layouts/application.html.erb").read
-        app_name = old_applayout.split("<title>")[1].split("</title>")[0]
-        app_name = "\n<h1>"+app_name+"</h1>\n"
-        tables = ActiveRecord::Base.connection.tables[1..-1]
-        list_items = []
-        tables.each do |t|
-          link = "<li><%=link_to '" + t.titleize + "'," + t + "_path%></li>"
-          list_items.push(link)
+      
+      
+      def gem_info
+        return if options.copy_css? || options.add_nav?
+              puts "
+              ----------------------------------------------------
+              Rtrain needs an argument to continue.
+              Please re-run the install generator with one of the
+              following options:
+              \n
+                 --copy_css
+                 --add_nav
+              \n
+              Refer to the README at https://github.com/lynnd335/rtrain
+              for details and illustrations of each option's executions
+              ----------------------------------------------------
+              "
         end
-        nav = "\n<div id=\"main-navbar\">\n\t<ul id=\"menu\">\n\t\t" + list_items.to_s + "\n\t</ul>\n</div>\t\n\n\n"
-        new_layout = old_applayout.split("<body>")
-        old_applayout = open("app/views/layouts/application.html.erb", "w")
-        old_applayout.write(new_layout[0] + "<body>" + app_name + nav + new_layout[1])
-        old_applayout.close
+
+      def run_rtrain
+        dir = File.dirname(__FILE__)
+        ##Begin Copy CSS
+
+        if options[:copy_css]
+          
+          scaffold_css = "../templates/rtrain_scaffold.scss"
+          new_css_path = File.join(dir, scaffold_css)
+          new_css = open(new_css_path).read
+
+          old_css = open("app/assets/stylesheets/scaffolds.scss", "w")
+          old_css.write(new_css)
+          old_css.close
+           puts "
+              ----------------------------------------------------
+              Rtrain Scaffold CSS now active in app/assets/stylesheets/
+              ----------------------------------------------------
+            "
+        end
+
+        ##end copy css
+        
+        ##begin add nvigation bar  
+
+        if options[:add_nav]
+
+          puts 'nav'
+
+          old_applayout = open("app/views/layouts/application.html.erb").read
+          app_name = old_applayout.split("<title>")[1].split("</title>")[0]
+          app_name = "\n<h1>"+app_name+"</h1>\n"
+          tables = ActiveRecord::Base.connection.tables[1..-1]
+          list_items = []
+
+            tables.each do |t|
+              link = "<li><%=link_to '" + t.titleize + "'," + t + "_path%></li>"
+              list_items.push(link)
+            end
+
+          nav = "\n<div id=\"main-navbar\">\n\t<ul id=\"menu\">\n\t\t" + list_items.join("\n\t\t") + "\n\t</ul>\n</div>\t\n\n\n"
+          new_layout = old_applayout.split("<body>")
+          old_applayout = open("app/views/layouts/application.html.erb", "w")
+          old_applayout.write(new_layout[0] + "<body>" + app_name + nav + new_layout[1])
+          old_applayout.close
+          ###
+          nav_css = "../templates/nav-bar.scss"
+          nav_css_path = File.join(dir, nav_css)
+          stylesheets = "app/assets/stylesheets/"
+          FileUtils.cp(nav_css_path, stylesheets)
+           puts "
+              ----------------------------------------------------
+              Rtrain Nav Bar and App Title now active in app/views/layouts/application.html.erb\n
+
+              IMPORTANT! If re-running this command, be sure to remove the HTML for the previous nav bar
+              as it will be duplicated within the text of app/views/layouts/application.html.erb.
+              ----------------------------------------------------
+            "
+        end  
+         
+        ##end add navigation bar
+
       end
     end
   end
